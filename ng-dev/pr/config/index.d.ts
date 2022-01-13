eaf25245c0a78d7e5720aaa2e1be5d51aea28c49
dymart <dylanmartin@ubiquitoustechnologies.io>
2022-01-13 14:25:25 -0800

    update for rnj 5

---
+++ ng-dev/pr/config/index.d.ts
@@ -0,0 +1,65 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { GithubConfig } from '../../utils/config';
+/**
+ * Possible merge methods supported by the Github API.
+ * https://developer.github.com/v3/pulls/#merge-a-pull-request-merge-button.
+ */
+export declare type GithubApiMergeMethod = 'merge' | 'squash' | 'rebase';
+/** Configuration for the Github API merge strategy. */
+export interface GithubApiMergeStrategyConfig {
+    /** Default method used for merging pull requests */
+    default: GithubApiMergeMethod;
+    /** Labels which specify a different merge method than the default. */
+    labels?: {
+        pattern: string;
+        method: GithubApiMergeMethod;
+    }[];
+}
+/** Configuration for the merge script. */
+export interface PullRequestConfig {
+    /**
+     * Configuration for the upstream remote. All of these options are optional as
+     * defaults are provided by the common dev-infra github configuration.
+     */
+    remote?: GithubConfig;
+    /** List of target labels. */
+    noTargetLabeling?: boolean;
+    /** Required base commits for given branches. */
+    requiredBaseCommits?: {
+        [branchName: string]: string;
+    };
+    /** Pattern that matches labels which imply a merge ready pull request. */
+    mergeReadyLabel: string | RegExp;
+    /** Label that is applied when special attention from the caretaker is required. */
+    caretakerNoteLabel?: string | RegExp;
+    /** Label which can be applied to fixup commit messages in the merge script. */
+    commitMessageFixupLabel: string | RegExp;
+    /**
+     * Whether pull requests should be merged using the Github API. This can be enabled
+     * if projects want to have their pull requests show up as `Merged` in the Github UI.
+     * The downside is that fixup or squash commits no longer work as the Github API does
+     * not support this.
+     */
+    githubApiMerge: false | GithubApiMergeStrategyConfig;
+    /**
+     * List of commit scopes which are exempted from target label content requirements. i.e. no `feat`
+     * scopes in patch branches, no breaking changes in minor or patch changes.
+     */
+    targetLabelExemptScopes?: string[];
+}
+/** Loads and validates the merge configuration. */
+export declare function assertValidPullRequestConfig<T>(config: T & Partial<{
+    pullRequest: PullRequestConfig;
+}>): asserts config is T & {
+    pullRequest: PullRequestConfig;
+};
+/** Label for pull requests containing a breaking change. */
+export declare const breakingChangeLabel = "flag: breaking change";
+/** Label for pull requests containing a deprecation. */
+export declare const deprecationLabel = "flag: deprecation";
---
+++ ng-dev/release/notes/release-notes.d.ts
@@ -0,0 +1,44 @@
+import * as semver from 'semver';
+import { CommitFromGitLog } from '../../commit-message/parse';
+import { GitClient } from '../../utils/git/git-client';
+/** Workspace-relative path for the changelog file. */
+export declare const workspaceRelativeChangelogPath = "CHANGELOG.md";
+/** Release note generation. */
+export declare class ReleaseNotes {
+    version: semver.SemVer;
+    private commits;
+    private git;
+    static forRange(git: GitClient, version: semver.SemVer, baseRef: string, headRef: string): Promise<ReleaseNotes>;
+    /** The RenderContext to be used during rendering. */
+    private renderContext;
+    /** The title to use for the release. */
+    private title;
+    /** The configuration ng-dev. */
+    private config;
+    /** The configuration for the release notes. */
+    private get notesConfig();
+    protected constructor(version: semver.SemVer, commits: CommitFromGitLog[], git: GitClient);
+    /** Retrieve the release note generated for a Github Release. */
+    getGithubReleaseEntry(): Promise<string>;
+    /** Retrieve the release note generated for a CHANGELOG entry. */
+    getChangelogEntry(): Promise<string>;
+    /**
+     * Prepend generated release note to the CHANGELOG.md file in the base directory of the repository
+     * provided by the GitClient. Removes entries for related prerelease entries as appropriate.
+     */
+    prependEntryToChangelogFile(): Promise<void>;
+    /** Retrieve the number of commits included in the release notes after filtering and deduping. */
+    getCommitCountInReleaseNotes(): Promise<number>;
+    /**
+     * Gets the URL fragment for the release notes. The URL fragment identifier
+     * can be used to point to a specific changelog entry through an URL.
+     */
+    getUrlFragmentForRelease(): Promise<string>;
+    /**
+     * Prompt the user for a title for the release, if the project's configuration is defined to use a
+     * title.
+     */
+    promptForReleaseTitle(): Promise<string | false>;
+    /** Build the render context data object for constructing the RenderContext instance. */
+    private generateRenderContext;
+}
